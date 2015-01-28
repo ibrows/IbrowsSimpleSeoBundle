47d2f7fe2e09e3f629f45c0fa859d3f652e00f98
Gladhon <marc.steiner@ibrows.ch>
2015-01-28 09:06:27 +0100

    add some entity create helper

--- Model/ContentManager.php
+++ Model/ContentManager.php
@@ -3,6 +3,8 @@
 namespace Ibrows\SimpleSeoBundle\Model;
 
 use Doctrine\ORM\EntityManager;
+use Ibrows\SimpleSeoBundle\Routing\KeyGenerator;
+use Symfony\Component\Routing\RouterInterface;
 
 class ContentManager implements ContentManagerInterface
 {
@@ -11,19 +13,54 @@ class ContentManager implements ContentManagerInterface
      * @var EntityManager
      */
     protected $em;
+
     /**
-     * @var
+     * @var string
      */
     protected $entityClass;
 
     /**
-     * @param EntityManager $em
-     * @param                             $entityClass
+     * @var KeyGenerator
      */
-    public function __construct(EntityManager $em, $entityClass)
+    protected $generator;
+
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+
+    /**
+     * @param EntityManager $em
+     * @param string $entityClass
+     * @param KeyGenerator $generator
+     * @param RouterInterface $router
+     */
+    public function __construct(EntityManager $em, $entityClass, KeyGenerator $generator, RouterInterface $router)
     {
         $this->em = $em;
+        $this->generator = $generator;
         $this->entityClass = $entityClass;
+        $this->router = $router;
+    }
+
+    /**
+     * @param $alias
+     * @param $path
+     * @param array $pathParameters
+     * @param null $locale
+     * @return ContentInterface
+     */
+    public function createNewAlias($alias, $path, array $pathParameters = array(), $locale = null)
+    {
+        $rc = new \ReflectionClass($this->entityClass);
+        $object = $rc->newInstance();
+        $object->setAlias($alias);
+        $path = $this->router->generate($path, $pathParameters);
+        $path = str_replace('/app_dev.php', '', $path);
+        $key = $this->generator->generateMetaTagKeyFromRelativePath($path, $this->router, $locale);
+        $object->setKeyword($key);
+        return $object;
     }
 
     /**
@@ -38,8 +75,17 @@ class ContentManager implements ContentManagerInterface
     /**
      * @return \Doctrine\ORM\EntityRepository
      */
-    protected function getRepository(){
+    protected function getRepository()
+    {
         return $this->em->getRepository($this->entityClass);
+    }
+
+    /**
+     * @param $alias
+     * @return null|object
+     */
+    public function findByAlias($alias){
+        return $this->getRepository()->findOneBy(array('alias'=>$alias));
     }
 
     /**
@@ -50,9 +96,9 @@ class ContentManager implements ContentManagerInterface
         $qb = $this->getRepository()->createQueryBuilder('a');
         $qb->select('a.pathinfo, a.alias');
         $qb->where('a.alias IS NOT null');
-        $qb->addOrderBy('a.id','ASC');
-        if($this->em->getClassMetadata($this->entityClass)->hasField('position')){
-            $qb->addOrderBy('a.position','ASC');
+        $qb->addOrderBy('a.id', 'ASC');
+        if ($this->em->getClassMetadata($this->entityClass)->hasField('position')) {
+            $qb->addOrderBy('a.position', 'ASC');
         }
         try {
             return $qb->getQuery()->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
@@ -69,10 +115,11 @@ class ContentManager implements ContentManagerInterface
      */
     public function findMetaTag($key, $locale = null)
     {
-        $criteria = array('keyword'=>$key);
-        if($locale && property_exists($this->entityClass,'locale')){
-            $criteria['locale']=$locale;
+        $criteria = array('keyword' => $key);
+        if ($locale && property_exists($this->entityClass, 'locale')) {
+            $criteria['locale'] = $locale;
         }
+
         return $this->getRepository()->findOneBy($criteria);
     }
 }
